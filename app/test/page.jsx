c5648a1eb0e5ea997e33eac11d1d21d7386b734c
anonymous-sidhax <72407218+anonymous-sidhax@users.noreply.github.com>
2025-02-15 14:13:01 -0600

    Added required images for the pages (#20)

---
+++ app/test/page.jsx
@@ -0,0 +1,8 @@
+export const metadata = {
+    title: "Test Page | Baar Baar NYC",
+    description: "This is a test page for Baar Baar NYC.",
+};
+
+export default function Test() {
+    return <h1>Testing</h1>;
+}
---
+++ components/Test.jsx
@@ -0,0 +1,7 @@
+"use client";
+
+import Image from "next/image";
+
+export default function Test() {
+  return (<h1>Hello</h1>);
+}
