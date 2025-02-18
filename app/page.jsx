20baafc33b0de05e58d9eca4d04b8e03b3c51a91
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2025-02-18 11:40:01 +0000

    fix: Created the CTA

--- app/page.jsx
+++ app/page.jsx
@@ -5,17 +5,17 @@ export default function Home() {
     return (
         <main className='min-h-screen'>
             <HomeSlider />
-            <div className='bg-accent h-48 flex justify-between items-center'>
-                <div className=''>
-                    <h1>Baar Baar Delivery and Pick Up</h1>
-                    <p>
+            <div className='bg-accent h-full flex flex-col justify-between items-center p-5 gap-4'>
+                <div className='text-white flex flex-col gap-4'>
+                    <h1 className='text-xl font-semibold text-center'>Baar Baar Delivery and Pick Up</h1>
+                    <p className='text-center'>
                         Baar Baar's Take Out menu is now available for delivery
                         through ChowNow and other Delivery platforms.
-                        Tuesday to Sunday - Dinner: 4:30pm to 10pm.
-                        For Pick up: email: delivery@baarbaarnyc.com or call 212.228.1200
                     </p>
+                    <p className='text-center'>Tuesday to Sunday - Dinner: 4:30pm to 10pm.</p>
+                    <p className='text-center'>For Pick up: email: delivery@baarbaarnyc.com or call 212.228.1200</p>
                 </div>
-                <button>Order Online</button>
+                <button className='text-md font-semibold text-white border-2 border-white px-3 py-0.5 rounded-sm hover:bg-accent transition-colors duration-200 ease-in'>Order Online</button>
             </div>
         </main>
     );
